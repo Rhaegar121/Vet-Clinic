fb10360ea9a15c3f88bab437adf3be88138dbe06
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-03-28 23:13:20 +0630

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -10,4 +10,19 @@ weight_kg DECIMAL,
 PRIMARY KEY(id)
 );
 
+/* Day 2 */
 ALTER TABLE animals ADD species VARCHAR(250);
+
+/* Day 3 */
+CREATE TABLE owners(
+id SERIAL PRIMARY KEY,
+full_name VARCHAR(250),
+age INT
+);
+CREATE TABLE species(
+id SERIAL PRIMARY KEY,
+name VARCHAR(250)
+);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
