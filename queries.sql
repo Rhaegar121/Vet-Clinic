6b62e6a9894a5e5a69d562bbf129abb9b993d216
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-03-28 23:19:03 +0630

    update queries.sql

--- queries.sql
+++ queries.sql
@@ -25,3 +25,13 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT name, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY name;
 SELECT name, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY name;
+
+/* Write Day 3 quries */
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT animals.name FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+SELECT owners.full_name, animals.name FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+SELECT o.full_name AS owner_name, a.name AS animal_name FROM owners AS o LEFT JOIN animals AS a ON o.id = a.owners_id;
+SELECT s.name AS species, COUNT(*) AS count FROM animals a JOIN species s ON a.species_id = s.id GROUP BY s.name;
+SELECT o.full_name AS owner_name, a.name AS animal_name FROM owners AS o JOIN animals AS a ON o.id = a.owners_id WHERE o.full_name = 'Jennifer Orwell' AND a.species_id = 2;
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+SELECT owners.full_name, COUNT(animals.id) AS num_animals FROM owners LEFT JOIN animals ON owners.id = animals.owners_id GROUP BY owners.id ORDER BY num_animals DESC LIMIT 1;
