fd462843af0d81bd4068135e95c7a55535e6e30c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-03-29 16:50:08 +0630

    insert data into specializations table

--- data.sql
+++ data.sql
@@ -37,3 +37,5 @@ UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Dean Wi
 
 
 INSERT INTO vets (name, age, date_of_graduation) VALUES ('Vet William Tatcher', 45, '2000-04-23'), ('Vet Maisy Smith', 26, '2019-01-17'), ('Vet Stephanie Mendez', 64, '1981-05-04'), ('Vet Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vets_id, species_id) VALUES (1, 1), (3, 2), (3, 1), (4, 2);
