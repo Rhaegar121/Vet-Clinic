9bbde6c26603bd57b8a2609d280eef57b431d27b
mohi <mohisadeghi302@gmail.com>
2023-04-04 15:08:53 +0430

    Update join table key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,19 +1,22 @@
 CREATE TABLE medical_histories(
-  id INT PRIMARY KEY,
+  id SERIAL not null PRIMARY KEY,
   patients_id INT REFERENCES patients (id),
   admitted_at TIMESTAMP,
   status VARCHAR(250)
 );
 
 CREATE TABLE treatments (
-  id INT PRIMARY KEY,
+  id SERIAL not null PRIMARY KEY,
   type VARCHAR(250),
   name VARCHAR(250)
 );
 
 CREATE TABLE medical_history_treatments (
-  medical_history_id INT REFERENCES REFERENCES medical_histories(id),,
-  treatment_id INT REFERENCES REFERENCES treatments(id),
+  medical_history_id INT,
+  treatment_id INT,
+  PRIMARY KEY (medical_history_id, treatment_id),
+  FOREIGN KEY medical_history_id INT REFERENCES REFERENCES medical_histories(id),,
+  FOREIGN KEY treatment_id INT REFERENCES REFERENCES treatments(id),
 );
 
 CREATE TABLE patients (
